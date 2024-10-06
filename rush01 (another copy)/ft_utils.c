fe8c8d405d7a7242ef57d7ca390560db9601f1eb
sleepylogin <168828625+sleepylogin@users.noreply.github.com>
2024-10-06 12:50:58 +0200

    Add files via upload

---
+++ rush01 (another copy)/ft_utils.c
@@ -0,0 +1,53 @@
+#include "rush01.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+	char	str[2];
+
+	if (nb < 0)
+	{
+		ft_putstr("-");
+		nb = -nb;
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	c = nb % 10 + '0';
+	str[0] = c; 
+	str[1] = '\0';
+	ft_putstr(str);
+}
+
+
+int	ft_check_arg(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if ((str[i] >= '1' && str[i] <= '4') || str[i] == ' ')
+		{
+			if (str[i] != ' ')
+				count++;
+		}
+		else
+			return (0);
+		i++;
+	}
+	return (count == 16);
+}
